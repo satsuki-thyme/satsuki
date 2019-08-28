4e30451718adb10eb591d5c1224e0960232f962e
Yayoi Thyme <12105444+yayoi-thyme@users.noreply.github.com>
2019-08-28 15:40:38 +0900

    change

--- distribution/narou-ruby-livedoor.js
+++
@@ -1,8 +0,0 @@
-<script>
-$(document).ready(function() {
-  $(document).find('.article-body-inner').each(function() {
-    var word = $(this).html().replace(/｜([^（]+?)《(.+?)》/g, '<ruby>$1<rt>$2</rt></ruby>').replace(/([\u4E00-\u9FFF]+?)（(.*?)）/g, '<ruby>$1<rt>$2</rt></ruby>').replace(/｜(（.*?）)/g, '$1')
-    $(this).html(word)
-  })
-})
-</script>
