fc04e424cd4dd4b9efcb604025c1313bcdd9bda7
yayoi thyme <yayoi.thyme@gmail.com>
2020-01-29 21:03:06 +0900

    write ad diary

--- mdparse.js
+++ mdparse.js
@@ -1,5 +1,22 @@
 function mdparse(src, opt) {
   let intmedi = ''
-  intmedi = src.replace(/^([^#*+-\\(\d\.\\)\\|=~_`\s\t]+)$/gm, '<p>$1</p>').replace(/^#{1}\s(.*)$/gm, '<h1>$1</h1>').replace(/^#{2}\s(.*)$/gm, '<h2>$1</h2>').replace(/^#{3}\s(.*)$/gm, '<h3>$1</h3>').replace(/^#{4}\s(.*)$/gm, '<h4>$1</h4>').replace(/^#{5}\s(.*)$/gm, '<h5>$1</h5>').replace(/^#{6}\s(.*)$/gm, '<h6>$1</h6>')
+  intmedi = src
+  .replace(/^(([^#*+-\\(\d\.\\)\\|=~_`\s\t].*?\n)+\n?$)/gm, '<p>$1</p>\n')
+  .replace(/^(?=\|[^|]*?)\|(?<!$)/gm, '<td>')
+  .replace(/^#{1}\s(.*)$/gm, '<h1>$1</h1>')
+  .replace(/^#{2}\s(.*)$/gm, '<h2>$1</h2>')
+  .replace(/^#{3}\s(.*)$/gm, '<h3>$1</h3>')
+  .replace(/^#{4}\s(.*)$/gm, '<h4>$1</h4>')
+  .replace(/^#{5}\s(.*)$/gm, '<h5>$1</h5>')
+  .replace(/^#{6}\s(.*)$/gm, '<h6>$1</h6>')
+  .replace(/^([\s\t]{1,})/gm, '')
+//  .replace(//gm, '')
+//  .replace(//gm, '')
+//  .replace(//gm, '')
+//  .replace(//gm, '')
+//  .replace(//gm, '')
+//  .replace(//gm, '')
+//  .replace(//gm, '')
+//  .replace(//gm, '')
   return intmedi
 }
