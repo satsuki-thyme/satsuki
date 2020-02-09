1ee91458bbdb16680af36d696d79ca4ecbb13bfc
yayoi thyme <yayoi.thyme@gmail.com>
2020-02-09 19:04:16 +0900

    word_replace kill

--- default.js
+++ default.js
@@ -1,6 +1,6 @@
-const search = $(location).attr('search').slice(1)
-const host = $(location).attr('host')
-const setting_array ={
+let search = location.search.slice(1)
+let host = location.hostname
+let setting_array ={
   "loadContents": {
     "yayoi-thyme.com": {
       "back_host": "raw.githubusercontent.com",
@@ -62,7 +62,7 @@ $(document).ready(function() {
 })
 $(function() {
   $(window).scroll(function() {
-    const scroll_amount = $(window).scrollTop()
+    let scroll_amount = $(window).scrollTop()
     if (scroll_amount > 0) {
       $('.up').fadeIn()
     } else if (scroll_amount <= 0) {
@@ -84,7 +84,7 @@ function loadContents(search, back_host, user_name, site_repository, branch, def
   let opus = ''
   let url = ''
   let extention = ''
-  const now = $.now()
+  let now = $.now()
   setUrl(search)
   .then(
     $.ajax({
@@ -104,7 +104,7 @@ function loadContents(search, back_host, user_name, site_repository, branch, def
     first_slash = search.indexOf('/')
     last_slash = search.lastIndexOf('/')
     opus = '/' + search.slice(first_slash + 1, last_slash - first_slash)
-    const file_name = '/' + search.slice(last_slash + 1)
+    let file_name = '/' + search.slice(last_slash + 1)
     if (search.length === 0) {
       extention = 'md'
       url = back_host + user_name + site_repository + branch + default_document
@@ -119,11 +119,11 @@ function loadContents(search, back_host, user_name, site_repository, branch, def
   function writeContents(data) {
     // Text
     if (extention === 'txt') {
-      const word = keywordReplace(mdparse(rubyParser(data)))
-      const char_len = data.replace(/([\s\t]*#.*?\r?\n|[\s\t]*\*.*|\r?\n|　)/g, '').replace(/｜(.*?)《.*?》/g, '$1').length
-      const insert_text = ''
-      const element_array = []
-      const story_len = Number(search.slice(last_slash + 1, last_period))
+      let word = mdparse(rubyParser(data))
+      let char_len = data.replace(/([\s\t]*#.*?\r?\n|[\s\t]*\*.*|\r?\n|　)/g, '').replace(/｜(.*?)《.*?》/g, '$1').length
+      let insert_text = ''
+      let element_array = []
+      let story_len = Number(search.slice(last_slash + 1, last_period))
       $('html').addClass('narou')
       $('div#novel_no').append(story_len + '/???')
       $('div#novel_honbun').append(word)
@@ -135,25 +135,25 @@ function loadContents(search, back_host, user_name, site_repository, branch, def
       prevNextLink(story_len)
     // HTML
     } else if (extention === 'html') {
-      const word = keywordReplace(data)
+      let word = data
       $('html').addClass('html')
       $('.html.contents-container').append(word)
       $('title').empty().append(decodeURI(text))
       $('.title').append(decodeURI(text))
     // Markdown
     } else if (extention === 'md') {
-      const word = keywordReplace(rubyParser(marked(data)))
+      let word = rubyParser(marked(data))
       $('html').addClass('markdown')
       $('.markdown.contents-container').append(marked(word))
     // Another
     } else {
-      const word = data
+      let word = data
       $('.another.contents-container').append(word)
     }
     dfd_load_contents.resolve()
     function prevNextLink(story_len) {
-      const prev_entity = ('000' + (story_len - 1)).slice(-3) + '.' + extention
-      const next_entity = ('000' + (story_len + 1)).slice(-3) + '.' + extention
+      let prev_entity = ('000' + (story_len - 1)).slice(-3) + '.' + extention
+      let next_entity = ('000' + (story_len + 1)).slice(-3) + '.' + extention
       let prev = ''
       let next = ''
       if (!isNaN(story_len)) {
@@ -174,25 +174,25 @@ function loadContents(search, back_host, user_name, site_repository, branch, def
   }
   function rubyParser(data) {
                    /* | ------------ルビとは無関係------------| */
-    const word = data.replace(/(^[\s\t]*#.*|[\s\t]*\*.*)/g, '').replace(/｜([^（]+?)《(.+?)》/g, '<ruby>$1<rt>$2</rt></ruby>').replace(/([\u4E00-\u9FFF]+?)（(.*?)）/g, '<ruby>$1<rt>$2</rt></ruby>').replace(/｜(（.*?）)/g, '$1')
+    let word = data.replace(/(^[\s\t]*#.*|[\s\t]*\*.*)/g, '').replace(/｜([^（]+?)《(.+?)》/g, '<ruby>$1<rt>$2</rt></ruby>').replace(/([\u4E00-\u9FFF]+?)（(.*?)）/g, '<ruby>$1<rt>$2</rt></ruby>').replace(/｜(（.*?）)/g, '$1')
     return word
   }
   function keywordReplace(data) {
-    const word_list_replace = [
+    let word_list_replace = [
                               ["\\{", '<span style="font-size: 50%; color: #999;">{</span><span>'],
                               ["\\}", '</span><span style="font-size: 50%; color: #999;">}</span>'],
                               ["(?<=\{)g(?=\})", '<span style="font-size: 50%; color: #00d;">いいところ</span>'],
                               ["(?<=\{)b(?=\})", '<span style="font-size: 50%; color: #d00;">わるいところ</span>'],
                               ["ｘ", '<span style="font-weight: bold; color: #cc0;">やめ</span>']
                             ]
-    const word_list_hide = ["_summary_", "_gist_"]
+    let word_list_hide = ["_summary_", "_gist_"]
     let word_list_replace_length = 0
     let work1 = data
     for (let i in word_list_replace) {
       word_list_replace_length++
     }
     for (let i = word_list_replace_length - 1; i >= 0; i--) {
-      const regexp1 = new RegExp(word_list_replace[i][0], 'g')
+      let regexp1 = new RegExp(word_list_replace[i][0], 'g')
       work1 = work1.replace(regexp1, word_list_replace[i][1])
     }
     for (let i = word_list_hide.length - 1; i >= 0; i--) {
@@ -205,12 +205,12 @@ function setUpLink() {
   $('.up button').css('right', ($(window).width() - $('.up').width()) / 2 + 5)
 }
 function setReturnLink(search, url_home) {
-  const last_period_search = search.lastIndexOf('.')
-  const first_slash_search = search.indexOf('/')
-  const last_slash_search = search.lastIndexOf('/')
-  const search_path_filenameless = search.slice(1, last_slash_search)
-  const search_path_filename_extentionless = search.slice(last_slash_search + 1, last_period_search)
-  const search_path_extention = search.slice(last_period_search + 1, last_period_search - last_slash_search)
+  let last_period_search = search.lastIndexOf('.')
+  let first_slash_search = search.indexOf('/')
+  let last_slash_search = search.lastIndexOf('/')
+  let search_path_filenameless = search.slice(1, last_slash_search)
+  let search_path_filename_extentionless = search.slice(last_slash_search + 1, last_period_search)
+  let search_path_extention = search.slice(last_period_search + 1, last_period_search - last_slash_search)
   if (search_path_filename_extentionless === 'index') {
     prependAnchor(url_home, '小説関連に戻る')
   } else {
