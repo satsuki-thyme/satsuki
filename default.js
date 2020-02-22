82bb7edf8e034986cb5b668180727345162a1966
yayoi thyme <yayoi.thyme@gmail.com>
2020-02-22 20:34:44 +0900

    update

--- default.js
+++ default.js
@@ -1,8 +1,21 @@
-$(function() {
+let windHeit = 0
+$(document).ready(function() {
   setUpLink()
+  setHeit()
+  $('.down button').on('click',function (e) {
+    $('html, body').animate({scrollTop: windHeit}, 'fast')
+  })
   $('.up button').on('click',function (e) {
     $('html, body').animate({scrollTop: 0}, 'fast')
   })
+})
+$(window).scroll(function() {
+  let scrlAmnt = $(window).scrollTop()
+  if (scrlAmnt < windHeit) {
+    $('.down').fadeIn()
+  } else if (scrlAmnt <= windHeit) {
+    $('.down').fadeOut()
+  }
 })
 $(window).scroll(function() {
   let scrlAmnt = $(window).scrollTop()
@@ -14,7 +27,11 @@ $(window).scroll(function() {
 })
 $(window).resize(function() {
   setUpLink()
+  setHeit()
 })
 function setUpLink() {
-  $('.up button').css('right', ($(window).width() - $('.up').width()) / 2 + 5)
+  $('.down button, .up button').css('right', ($(window).width() - $('.up').width()) / 2 + 5)
+}
+function setHeit() {
+  windHeit = $('body').outerHeight()
 }
