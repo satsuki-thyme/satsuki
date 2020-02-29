e5e852b50a140da158ccf89f73de1f9ad8809db4
yayoi thyme <yayoi.thyme@gmail.com>
2020-03-01 05:44:42 +0900

    update

--- default.js
+++ default.js
@@ -3,11 +3,15 @@ scrlAmnt = 0
 $(document).ready(function() {
   setUpLink()
   setHeit()
+  setReldBtn()
   $('.down button').on('click',function (e) {
     $('html, body').animate({scrollTop: scrlHeit + 100}, 'fast')
   })
   $('.up button').on('click',function (e) {
     $('html, body').animate({scrollTop: 0}, 'fast')
+  })
+  $('.reload button').on('click', function(e) {
+    location.reload()
   })
 })
 $(window).scroll(function() {
@@ -31,8 +35,13 @@ $(window).resize(function() {
   setHeit()
 })
 function setUpLink() {
-  $('.down button, .up button').css('right', ($(window).width() - $('.up').width()) / 2 + 5)
+  $('.down button, .up button, .reload button').css('right', ($(window).width() - $('.up').width()) / 2 + 5)
 }
 function setHeit() {
   scrlHeit = $('header').outerHeight(true) + $('main').outerHeight(true) + $('footer').outerHeight(true) - window.outerHeight - 2
+}
+function setReldBtn() {
+  if (location.search.indexOf('preview=1') >= 0) {
+    $('.reload').css('display', 'block')
+  }
 }
