a6c4feb9c132f51f4dd6ed9b761c12eefeaa9514
satsuki-thyme <satsuki.thyme@gmail.com>
2026-03-26 03:21:19 +0900

    気まぐれ

--- handler.js
+++ handler.js
@@ -1747,7 +1747,7 @@ Promise.all([
                       marksEnclosure,
                       `delete`
                     ),
-                    "newLineMode": `few`,
+                    "newLineMode": `raw`,
                     "rubyMode": `parse`,
                     "parenthesis": `normal`,
                     "comment": `delete-together`
